9624fcfb754d070503844a6ff5642f66ddf90c49
Juan Gonzalez <cjotaqua@gmail.com>
2026-07-25 22:46:40 -0600

    Simplify Railway container start

---
+++ railway/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+
+APP_PORT="$(printf '%s' "${PORT:-10000}" | sed 's/[^0-9].*$//')"
+if [ -z "$APP_PORT" ]; then
+    APP_PORT=10000
+fi
+
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+exec php -S 0.0.0.0:"$APP_PORT" -t public public/index.php
